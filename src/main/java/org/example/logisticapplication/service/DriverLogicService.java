35d99acd195674ff901d4f416322ffad011da79b
Sergey <102069981+SergTito@users.noreply.github.com>
2024-11-29 18:39:35 +0300

    Update DriverLogicService.java
    @Service

--- src/main/java/org/example/logisticapplication/service/DriverLogicService.java
+++ src/main/java/org/example/logisticapplication/service/DriverLogicService.java
@@ -3,6 +3,7 @@ package org.example.logisticapplication.service;
 import org.example.logisticapplication.domain.Driver.Driver;
 import org.example.logisticapplication.domain.Truck.Truck;
 
+@Service
 public interface DriverLogicService {
 
     Driver addTruckForDriver(Long driverId, Long truck);
